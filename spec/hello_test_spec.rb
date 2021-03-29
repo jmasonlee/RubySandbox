7b1700642b2a26c8bec549481a4ae62107bfe633
Jacqueline Lee <jmasonlee@gmail.com>
2021-03-29 01:30:33 +0000

    Add rubocop

--- spec/hello_test_spec.rb
+++ spec/hello_test_spec.rb
@@ -1,8 +1,8 @@
-require 'rspec'
-require 'hello'
+require "rspec"
+require "hello"
 
-describe 'Hello' do
-  context 'when run without arguments' do
+describe "Hello" do
+  context "when run without arguments" do
     it 'returns "Hello, World!"' do
       expect(Hello.hello).to eq("Hello, World!")
     end
